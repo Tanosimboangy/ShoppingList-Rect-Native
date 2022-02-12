5630ad20884cdb29fb13bdb4d11b9b697eaa5a2d
Tanosimboangy <Jacquit.aja@onja.org>
2022-02-12 09:54:15 +0300

    accomplishing this small app

---
+++ components/AddItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Icon from "react-native-vector-icons/dist/FontAwesome"
+
+const AddItem = ({addItem, text, addText}) => {
+
+  return (
+    <View>
+      <TextInput placeholder="Add item..." style={styles.input} onChangeText={addText}/>
+      <TouchableOpacity style={styles.btn} onPress={() => addItem(text)}>
+        <Text style={styles.btnText}><Icon name="plus" size={20} />Add Item</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  input: {
+      height: 60,
+      padding: 8,
+      fontSize: 16,
+  },
+  btn: {
+      backgroundColor: "#c2bad8",
+      padding: 9,
+      margin: 5,
+  },
+  btnText: {
+      color: "darkslateblue",
+      fontSize: 20,
+      textAlign: "center"
+  }
+})
+
+export default AddItem;
